b9480e0b6fd4809961217f74318bc3b1e55f31a0
SergeiNA <nikseas@yandex.ru>
2019-11-15 18:30:39 +0300

    Fix bugs add fact and fibb computations

--- allocator.cpp
+++ allocator.cpp
@@ -16,34 +16,39 @@ CustomContaiter<hard,block_allocator<Node<hard>,N>> name;
 
 int main(int, char *[])
 {
+    const size_t COUNT =11;
+    std::vector<std::pair<size_t,size_t>> FibFact(COUNT);
+    formFiFa<COUNT>(FibFact);
     {
-        //    std::map<int, hard, std::less<int>> map_std;
-        //    for (size_t i = 0; i < 11; ++i)
-        //        map_std.try_emplace(i, i, i);
+
+           std::map<int, hard, std::less<int>> map_std;
+           for (size_t i = 0; i < COUNT; ++i)
+               map_std.try_emplace(i, FibFact[i].first, FibFact[i].second);
 
         // allocations of std::stack<T*> stack_ptr ;
         // libstdc++.so.6->malloc(64)
         // libstdc++.so.6->malloc(512)
 
         std::map<int, hard, std::less<int>, block_allocator<std::pair<const int, hard>, 10>> map_block;
-        for (size_t i = 0; i < 11; ++i)
-            map_block.try_emplace(i, i, i);
+        for (size_t i = 0; i < COUNT; ++i)
+            map_block.try_emplace(i, FibFact[i].first, FibFact[i].second);
 
         for (const auto &el : map_block)
             std::cout << el.second.getValues() << std::endl;
+
     }
 
-    std::cout << "------------------------\n";
+    std::cout << "Custom container custom alloc ----------------\n";
     {
-        // CustomContaiter<hard> std_cont_hard;
-        // for (size_t i = 0; i < 11; ++i)
-        //     std_cont_hard.try_push(i,i);
+        CustomContaiter<hard> std_cont_hard;
+        for (size_t i = 0; i < COUNT; ++i)
+            std_cont_hard.try_push(FibFact[i].first, FibFact[i].second);
 
         CustomContaiter<hard, block_allocator<Node<hard>, 10>> block_cont_hard;
-        for (size_t i = 0; i < 11; ++i)
-            block_cont_hard.try_push(i, i);
+        for (size_t i = 0; i < COUNT; ++i)
+            block_cont_hard.try_push(FibFact[i].first, FibFact[i].second);
 
-        // if in public    std::cout<<el.value.i<<' '<<el.value.d<<std::endl; works fine
+        // // if in public    std::cout<<el.value.i<<' '<<el.value.d<<std::endl; works fine
         for (const auto &el : block_cont_hard)
         {
             std::cout << el.value.getValues() << std::endl;
@@ -83,19 +88,14 @@ int main(int, char *[])
     {
         std::cout << "---" << std::endl;
         CustomContaiter<easy, block_allocator<Node<easy>, 5>> init_easy;
-        init_easy.try_push(1, 2);
-        init_easy.try_push(1, 2);
-        init_easy.try_push(1, 2);
-        init_easy.try_push(1, 2);
-        std::cout << "---" << std::endl;
+        for (size_t i = 0; i < 3; i++)
+        {
+            init_easy.try_push(FibFact[i].first, FibFact[i].second);
+        }
+        
         CustomContaiter<easy> std_easy(init_easy);
-        std::cout << "---" << std::endl;
         CustomContaiter<easy, block_allocator<Node<easy>, 2>> move_easy(std_easy);
     }
 
     return 0;
 }
-
-/*
-h.emplace(std::piecewise_construct, std::forward_as_tuple(i), std::forward_as_tuple(i, i));
-*/
--- utility.h
+++ utility.h
@@ -1,8 +1,45 @@
 #pragma once
 #include <utility>
 #include <ostream>
+#include <vector>
 
 template <class T, class U>
 std::ostream& operator << (std::ostream& os, std::pair<T,U> p){
     return os<<p.first<<' '<<p.second;
 }
+
+template<size_t N>
+size_t fib(){
+    return fib<N-2>()+fib<N-1>();
+}
+
+template<>
+size_t fib<0>(){return 1;};
+
+template<>
+size_t fib<1>(){return 1;};
+
+template<size_t N>
+size_t fact(){
+    return N*fact<N-1>();
+}
+
+template<>
+size_t fact<0>(){
+    return 1;
+}
+
+template <size_t N>
+void formFiFa(std::vector<std::pair<size_t,size_t>>& arr){
+    arr[N-1].first = fib<N-1>();
+    arr[N-1].second = fact<N-1>();
+    formFiFa<N-1>(arr);
+}
+
+template<>
+void formFiFa<0>(std::vector<std::pair<size_t,size_t>>& arr){
+    arr[0].first = fib<0>();
+    arr[0].second = fact<0>();
+}
+
+
